2d81ba23964d72896e5f7952540525ac7e19aaca
Hmaster17 <1734rithvika@gmail.com>
2021-01-14 20:34:55 +0530

    WhiteNoise

--- Nirvata_Django/settings.py
+++ Nirvata_Django/settings.py
@@ -24,9 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 't!z30+r+s&8pv8ff)ed=!h2)o(i18#)yiwicff579lenr5%zj7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['nirvata.herokuapp.com','127.0.0.1']
+
 
 
 # Application definition
@@ -43,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -118,7 +120,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'Nirvata/staticfiles')
 STATIC_URL = '/static/'
 MEDIA_URL = '/images/'
+STATICFILES_DIRS=[
+    os.pathjoin(BASE_DIR, 'Nirvata/static')
+]
 MEDIA_ROOT = os.path.join(BASE_DIR, 'Nirvata/static/Nirvata/images')
